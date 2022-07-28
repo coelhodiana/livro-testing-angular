5df60ff27babc1e63d05912c2a55d21dffb2e766
coelhodiana <coelhodiana04@gmail.com>
2022-07-27 20:52:50 -0400

    feat: teste de integração de component dependendo de serviço

---
+++ src/app/components/service-counter/service-counter.component.spec.ts
@@ -0,0 +1,47 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CounterService } from '../../services/counter.service';
+import { click, expectText, setFieldValue } from '../../spec-helpers/element.spec-helper';
+import { ServiceCounterComponent } from './service-counter.component';
+
+describe('', () => {
+  let component: ServiceCounterComponent;
+  let fixture: ComponentFixture<ServiceCounterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ServiceCounterComponent],
+      providers: [CounterService]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ServiceCounterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('shows the start count', () => {
+    expectText(fixture, 'count', '0');
+  });
+
+  it('increments the count', () => {
+    click(fixture, 'increment-button');
+    fixture.detectChanges();
+    expectText(fixture, 'count', '1');
+  });
+
+  it('decrements the count', () => {
+    click(fixture, 'decrement-button');
+    fixture.detectChanges();
+    expectText(fixture, 'count', '-1');
+  });
+
+  it('resets the count', () => {
+    const newCount = 456;
+    setFieldValue(fixture, 'reset-input', String(newCount));
+    click(fixture, 'reset-button');
+    fixture.detectChanges();
+    expectText(fixture, 'count', String(newCount));
+  });
+});
